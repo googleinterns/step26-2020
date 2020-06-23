02e010d7e85345d26632897972bb735134bf225d
Cody Rivera <codyjrivera@google.com>
2020-06-23 23:32:40 +0000

    Remove traces of second page

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -18,11 +18,10 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MaterialComponents} from './material-components';
-import {TestPageComponent} from './test-page/test-page.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, TestPageComponent],
+  declarations: [AppComponent],
   imports: [
     MaterialComponents,
     BrowserModule,

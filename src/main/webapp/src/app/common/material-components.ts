080b2ed404ace52275deda4c98d1e15b206047c7
Cody Rivera <codyjrivera@google.com>
2020-06-29 21:48:50 +0000

    Integrate schedule-page and user-profile page

--- src/main/webapp/src/app/common/material-components.ts
+++ src/main/webapp/src/app/common/material-components.ts
@@ -17,8 +17,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  exports: [MatToolbarModule, MatIconModule, MatInputModule, MatCardModule],
+  exports: [MatToolbarModule, MatIconModule, MatInputModule, MatCardModule, MatButtonModule],
 })
 export class MaterialComponents {}

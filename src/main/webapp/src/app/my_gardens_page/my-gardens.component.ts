8c05ef1f7e1b27326f45aaf497a38f1a06bd4e26
Kayla Harwell <kayla.h.7556@gmail.com>
2020-07-08 19:24:53 +0000

    fix nit issues regarding comments and move button css to common css file

--- src/main/webapp/src/app/my_gardens_page/my-gardens.component.ts
+++ src/main/webapp/src/app/my_gardens_page/my-gardens.component.ts
@@ -4,11 +4,14 @@ import {OwlOptions} from 'ngx-owl-carousel-o';
 @Component({
   selector: 'MyGardens',
   templateUrl: './my-gardens.component.html',
-  styleUrls: ['./my-gardens.component.css'],
+  styleUrls: [
+    './my-gardens.component.css',
+    '../common/growpod-page-styles.css',
+  ],
 })
 export class MyGardensComponent implements OnInit {
-  /* Option to display a certian number of slides based off of browser window size along with
-   ** some style options
+  /** Option to display a certain number of slides based off of browser window size along with
+   * some style options
    */
   customOptions: OwlOptions = {
     responsive: {

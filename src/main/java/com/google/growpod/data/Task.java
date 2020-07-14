e31151759ae16b4190330aba00a00144863d518d
Stephanie <galvanstephanie2018@gmail.com>
2020-07-14 13:57:37 +0000

    Task and TaskServlet created for mockup info

---
+++ src/main/java/com/google/growpod/data/Task.java
@@ -0,0 +1,70 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.growpod.data;
+
+import java.util.List;
+
+/** Task data class * */
+public class Task {
+
+  /* The title of the event */
+  private String title;
+
+  /* Start time for the event */
+  private String time;
+
+  /* List of people needed on the event */
+  private List<String> participants;
+
+  /* The task's detailed description. Optional */
+  private String description;
+
+  public Task(String title, String time, List<String> participants, String description) {
+    this.title = title;
+    this.time = time;
+    this.participants = participants;
+    this.description = description;
+  }
+
+  /* Getters */
+  public String getTitle() {
+    return this.title;
+  }
+
+  public String getTime() {
+    return this.time;
+  }
+
+  public List<String> getParticipants() {
+    return this.participants;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  /* Setters */
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public void setTime(String time) {
+    this.title = title;
+  }
+
+  public void setDescription(String description) {
+    this.title = title;
+  }
+}
---
+++ src/main/java/com/google/growpod/servlets/TaskServlet.java
@@ -0,0 +1,63 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.growpod.servlets;
+
+import com.google.growpod.data.Task;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({"/task"})
+public class TaskServlet extends HttpServlet {
+
+  private List<Task> tasks;
+
+  @Override
+  public void init() {
+    tasks = new ArrayList<>();
+    List<String> hosts = new ArrayList();
+    List<String> interns = new ArrayList();
+
+    hosts.add("Caroline");
+    hosts.add("Jake");
+
+    interns.add("Stephanie");
+    interns.add("Kayla");
+    interns.add("Cody");
+
+    tasks.add(
+        new Task("Water plants", "8:30 am", hosts, "Don't forget to water the trees as well"));
+    tasks.add(
+        new Task(
+            "Pick up apples",
+            "3:00 pm",
+            interns,
+            "Feel free to grab a few for yourself, but please put them in their assigned boxes"));
+    tasks.add(new Task("Send apples to the shelter", "4:30 pm", hosts, "Shelter closes at 5:30pm"));
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Task task = tasks.get((int) (Math.random() * tasks.size()));
+
+    response.setContentType("text/html;");
+    response.getWriter().println(new Gson().toJson(task));
+  }
+}

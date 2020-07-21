6dc168b121bd2ed786facdaf5a22e2c6e1039db0
Stephanie <galvanstephanie2018@gmail.com>
2020-07-21 13:58:00 +0000

    User repository for local storage (client side)

---
+++ src/main/webapp/src/repositories/model/user.ts
@@ -0,0 +1,13 @@
+/* User class for gapi authentication */
+export class User {
+    
+    id:string;
+    email:string;
+
+    static fromBasicProfile(profile: gapi.auth2.BasicProfile): User {
+        let user:User = new User();
+        user.id = profile.getId();
+        user.email = profile.getEmail();
+        return user;
+    }
+}
